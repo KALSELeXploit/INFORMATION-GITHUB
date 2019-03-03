e3f7e47cb443f537628cfa14eebea4f6691340e5
KALSELeXploit <48057395+KALSELeXploit@users.noreply.github.com>
2019-03-03 14:01:40 +0800

    Add files via upload

---
+++ clay.php
@@ -0,0 +1,15 @@
+<?php
+echo "
+   mm.           dM8
+   YMMMb.       dMM8
+    YMMMMb     dMMM'
+     YMMMb   dMMMP
+       YMMM  MMM'
+          MbdMP
+      .dMMMMMM.P   -=[DEFACE SCRIPT CREATOR]=-
+     dMM  MMMMMMM  -=[Coded By Muhammad Rafli]=-
+     8MMMMMMMMMMI  -=[Team : Kalsel[E]Xploit]=-
+      YMMMMMMMMM   -=[Telegram : RabbitCL4Y IG]=-
+        MMMMMMP
+       MxM .mmm\n";
+?>
